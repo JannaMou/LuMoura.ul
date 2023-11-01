0a257551307e007ed4b9fe2baf1873c57502d90c
DanielNogueira8859 <148869640+DanielNogueira8859@users.noreply.github.com>
2023-11-01 08:25:32 -0300

    update

--- LuMoura.ul/AgendarHorario.cs
+++ LuMoura.ul/AgendarHorario.cs
@@ -59,14 +59,13 @@ namespace LuMoura.ul
         private void textDescricao_TextChanged(object sender, EventArgs e)
         {
             Agendar agendar = new Agendar();
-            Agendar.agendar(textNome.Text, textTelefone.Text,comboServiço.Text, textDescricao.Text);
+            agendar.agendar(textNome.Text, textTelefone.Text,comboServiço.Text, textDescricao.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Agendar horario = new Agendar();
-            horario.Cadastrar(textNome.Text, textBox2.Text,TxtEmail.Text);
-            MessageBox.Show("Cadastrado");
+           
 
         }
 
