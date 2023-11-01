e14c762d29c983e9af4254da1ebf7c2dbcf7c0d3
Alexsaglia <132571874+Alexsaglia@users.noreply.github.com>
2023-11-01 08:25:56 -0300

    update

--- LuMoura.ul/Program.cs
+++ LuMoura.ul/Program.cs
@@ -11,7 +11,7 @@ namespace LuMoura.ul
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AgendarHorario());
+            Application.Run(new AreaCliente_Cadastro());
         }
     }
 }
