62c00395dce5a171691386bcb101c2e86fcc7ac6
Alexsaglia <132571874+Alexsaglia@users.noreply.github.com>
2023-11-01 08:15:31 -0300

    update

---
+++ LuMoura.ul/AreaCliente_Cadastro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuMoura.ul
+{
+    public partial class AreaCliente_Cadastro : Form
+    {
+        public AreaCliente_Cadastro()
+        {
+            InitializeComponent();
+        }
+    }
+}
