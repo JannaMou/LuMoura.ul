6ae21106a04f8a092a7753dfc1fbed67c8898570
Alexsaglia <132571874+Alexsaglia@users.noreply.github.com>
2023-11-10 10:38:18 -0300

    update

---
+++ LuMoura.ul/ControleCadastroADM.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace LuMoura.ul
+{
+    partial class ControleCadastroADM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            TxtCodigo = new TextBox();
+            dataGridView1 = new DataGridView();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            TxtEmail = new TextBox();
+            label7 = new Label();
+            TxtTelefone = new TextBox();
+            label6 = new Label();
+            label1 = new Label();
+            TxtNome = new TextBox();
+            label2 = new Label();
+            TxtCPF = new TextBox();
+            BtMenu = new Guna.UI2.WinForms.Guna2CircleButton();
+            BtCadastrar = new Guna.UI2.WinForms.Guna2Button();
+            BtBuscar = new Guna.UI2.WinForms.Guna2Button();
+            BtAtualizar = new Guna.UI2.WinForms.Guna2Button();
+            BtExcluir = new Guna.UI2.WinForms.Guna2Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(BtExcluir);
+            groupBox1.Controls.Add(BtAtualizar);
+            groupBox1.Controls.Add(BtBuscar);
+            groupBox1.Controls.Add(BtCadastrar);
+            groupBox1.Controls.Add(BtMenu);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(TxtCodigo);
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(TxtEmail);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(TxtTelefone);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(TxtNome);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(TxtCPF);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1207, 385);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(767, 61);
+            label4.Name = "label4";
+            label4.Size = new Size(23, 15);
+            label4.TabIndex = 28;
+            label4.Text = "Id :";
+            // 
+            // TxtCodigo
+            // 
+            TxtCodigo.Location = new Point(794, 56);
+            TxtCodigo.Name = "TxtCodigo";
+            TxtCodigo.Size = new Size(47, 23);
+            TxtCodigo.TabIndex = 27;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(391, 163);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(650, 173);
+            dataGridView1.TabIndex = 26;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Lu;
+            pictureBox1.Location = new Point(107, 62);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(204, 279);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 23;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(347, 121);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 21;
+            label3.Text = "E-mail :";
+            // 
+            // TxtEmail
+            // 
+            TxtEmail.Location = new Point(391, 117);
+            TxtEmail.Name = "TxtEmail";
+            TxtEmail.Size = new Size(324, 23);
+            TxtEmail.TabIndex = 22;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(552, 89);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Telefone :";
+            // 
+            // TxtTelefone
+            // 
+            TxtTelefone.Location = new Point(615, 86);
+            TxtTelefone.Name = "TxtTelefone";
+            TxtTelefone.Size = new Size(100, 23);
+            TxtTelefone.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Emoji", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.Location = new Point(552, 15);
+            label6.Name = "label6";
+            label6.Size = new Size(267, 32);
+            label6.TabIndex = 10;
+            label6.Text = "FICHA DE CADASTRO ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(347, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome :";
+            // 
+            // TxtNome
+            // 
+            TxtNome.Location = new Point(391, 57);
+            TxtNome.Name = "TxtNome";
+            TxtNome.Size = new Size(324, 23);
+            TxtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(359, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 15);
+            label2.TabIndex = 2;
+            label2.Text = "CPF :";
+            // 
+            // TxtCPF
+            // 
+            TxtCPF.Location = new Point(391, 86);
+            TxtCPF.Name = "TxtCPF";
+            TxtCPF.Size = new Size(100, 23);
+            TxtCPF.TabIndex = 3;
+            // 
+            // BtMenu
+            // 
+            BtMenu.DisabledState.BorderColor = Color.DarkGray;
+            BtMenu.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtMenu.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtMenu.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtMenu.FillColor = Color.Fuchsia;
+            BtMenu.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            BtMenu.ForeColor = Color.White;
+            BtMenu.Location = new Point(24, 22);
+            BtMenu.Name = "BtMenu";
+            BtMenu.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            BtMenu.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            BtMenu.Size = new Size(74, 43);
+            BtMenu.TabIndex = 30;
+            BtMenu.Text = "Menu";
+            // 
+            // BtCadastrar
+            // 
+            BtCadastrar.CustomizableEdges = customizableEdges7;
+            BtCadastrar.DisabledState.BorderColor = Color.DarkGray;
+            BtCadastrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtCadastrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtCadastrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtCadastrar.FillColor = Color.Fuchsia;
+            BtCadastrar.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtCadastrar.ForeColor = Color.White;
+            BtCadastrar.Location = new Point(754, 117);
+            BtCadastrar.Name = "BtCadastrar";
+            BtCadastrar.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            BtCadastrar.Size = new Size(110, 26);
+            BtCadastrar.TabIndex = 31;
+            BtCadastrar.Text = "Cadastrar";
+            // 
+            // BtBuscar
+            // 
+            BtBuscar.CustomizableEdges = customizableEdges5;
+            BtBuscar.DisabledState.BorderColor = Color.DarkGray;
+            BtBuscar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtBuscar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtBuscar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtBuscar.FillColor = Color.Fuchsia;
+            BtBuscar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtBuscar.ForeColor = Color.White;
+            BtBuscar.Location = new Point(847, 56);
+            BtBuscar.Name = "BtBuscar";
+            BtBuscar.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            BtBuscar.Size = new Size(96, 23);
+            BtBuscar.TabIndex = 32;
+            BtBuscar.Text = "Buscar";
+            // 
+            // BtAtualizar
+            // 
+            BtAtualizar.CustomizableEdges = customizableEdges3;
+            BtAtualizar.DisabledState.BorderColor = Color.DarkGray;
+            BtAtualizar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtAtualizar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtAtualizar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtAtualizar.FillColor = Color.Fuchsia;
+            BtAtualizar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtAtualizar.ForeColor = Color.White;
+            BtAtualizar.Location = new Point(949, 56);
+            BtAtualizar.Name = "BtAtualizar";
+            BtAtualizar.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            BtAtualizar.Size = new Size(96, 23);
+            BtAtualizar.TabIndex = 33;
+            BtAtualizar.Text = "Atualizar";
+            // 
+            // BtExcluir
+            // 
+            BtExcluir.CustomizableEdges = customizableEdges1;
+            BtExcluir.DisabledState.BorderColor = Color.DarkGray;
+            BtExcluir.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtExcluir.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtExcluir.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtExcluir.FillColor = Color.Fuchsia;
+            BtExcluir.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtExcluir.ForeColor = Color.White;
+            BtExcluir.Location = new Point(1051, 56);
+            BtExcluir.Name = "BtExcluir";
+            BtExcluir.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            BtExcluir.Size = new Size(96, 23);
+            BtExcluir.TabIndex = 34;
+            BtExcluir.Text = "Excluir";
+            // 
+            // ControleCadastroADM
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1231, 450);
+            Controls.Add(groupBox1);
+            Name = "ControleCadastroADM";
+            Text = "ControleCadastroADM";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Guna.UI2.WinForms.Guna2Button BtExcluir;
+        private Guna.UI2.WinForms.Guna2Button BtAtualizar;
+        private Guna.UI2.WinForms.Guna2Button BtBuscar;
+        private Guna.UI2.WinForms.Guna2Button BtCadastrar;
+        private Guna.UI2.WinForms.Guna2CircleButton BtMenu;
+        private Label label4;
+        private TextBox TxtCodigo;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private TextBox TxtEmail;
+        private Label label7;
+        private TextBox TxtTelefone;
+        private Label label6;
+        private Label label1;
+        private TextBox TxtNome;
+        private Label label2;
+        private TextBox TxtCPF;
+    }
+}
