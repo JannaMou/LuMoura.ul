141a2003fcf398647c97e52a3f4fc65f154f8da3
Alexsaglia <132571874+Alexsaglia@users.noreply.github.com>
2023-11-10 10:53:05 -0300

    update

--- LuMoura.ul/ControleCadastroADM.Designer.cs
+++ LuMoura.ul/ControleCadastroADM.Designer.cs
@@ -28,16 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             groupBox1 = new GroupBox();
+            BtExcluir = new Guna.UI2.WinForms.Guna2Button();
+            BtAtualizar = new Guna.UI2.WinForms.Guna2Button();
+            BtBuscar = new Guna.UI2.WinForms.Guna2Button();
+            BtCadastrar = new Guna.UI2.WinForms.Guna2Button();
+            BtMenu = new Guna.UI2.WinForms.Guna2CircleButton();
             label4 = new Label();
             TxtCodigo = new TextBox();
             dataGridView1 = new DataGridView();
@@ -51,11 +56,6 @@
             TxtNome = new TextBox();
             label2 = new Label();
             TxtCPF = new TextBox();
-            BtMenu = new Guna.UI2.WinForms.Guna2CircleButton();
-            BtCadastrar = new Guna.UI2.WinForms.Guna2Button();
-            BtBuscar = new Guna.UI2.WinForms.Guna2Button();
-            BtAtualizar = new Guna.UI2.WinForms.Guna2Button();
-            BtExcluir = new Guna.UI2.WinForms.Guna2Button();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -63,6 +63,7 @@
             // 
             // groupBox1
             // 
+            groupBox1.BackColor = Color.FromArgb(255, 192, 255);
             groupBox1.Controls.Add(BtExcluir);
             groupBox1.Controls.Add(BtAtualizar);
             groupBox1.Controls.Add(BtBuscar);
@@ -87,12 +88,102 @@
             groupBox1.TabIndex = 14;
             groupBox1.TabStop = false;
             // 
+            // BtExcluir
+            // 
+            BtExcluir.CustomizableEdges = customizableEdges1;
+            BtExcluir.DisabledState.BorderColor = Color.DarkGray;
+            BtExcluir.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtExcluir.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtExcluir.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtExcluir.FillColor = Color.Fuchsia;
+            BtExcluir.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtExcluir.ForeColor = Color.White;
+            BtExcluir.Location = new Point(1051, 56);
+            BtExcluir.Name = "BtExcluir";
+            BtExcluir.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            BtExcluir.Size = new Size(96, 23);
+            BtExcluir.TabIndex = 34;
+            BtExcluir.Text = "Excluir";
+            BtExcluir.Click += BtExcluir_Click;
+            // 
+            // BtAtualizar
+            // 
+            BtAtualizar.CustomizableEdges = customizableEdges3;
+            BtAtualizar.DisabledState.BorderColor = Color.DarkGray;
+            BtAtualizar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtAtualizar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtAtualizar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtAtualizar.FillColor = Color.Fuchsia;
+            BtAtualizar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtAtualizar.ForeColor = Color.White;
+            BtAtualizar.Location = new Point(949, 56);
+            BtAtualizar.Name = "BtAtualizar";
+            BtAtualizar.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            BtAtualizar.Size = new Size(96, 23);
+            BtAtualizar.TabIndex = 33;
+            BtAtualizar.Text = "Atualizar";
+            BtAtualizar.Click += BtAtualizar_Click;
+            // 
+            // BtBuscar
+            // 
+            BtBuscar.CustomizableEdges = customizableEdges5;
+            BtBuscar.DisabledState.BorderColor = Color.DarkGray;
+            BtBuscar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtBuscar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtBuscar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtBuscar.FillColor = Color.Fuchsia;
+            BtBuscar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtBuscar.ForeColor = Color.White;
+            BtBuscar.Location = new Point(847, 56);
+            BtBuscar.Name = "BtBuscar";
+            BtBuscar.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            BtBuscar.Size = new Size(96, 23);
+            BtBuscar.TabIndex = 32;
+            BtBuscar.Text = "Buscar";
+            BtBuscar.Click += BtBuscar_Click;
+            // 
+            // BtCadastrar
+            // 
+            BtCadastrar.CustomizableEdges = customizableEdges7;
+            BtCadastrar.DisabledState.BorderColor = Color.DarkGray;
+            BtCadastrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtCadastrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtCadastrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtCadastrar.FillColor = Color.Fuchsia;
+            BtCadastrar.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtCadastrar.ForeColor = Color.White;
+            BtCadastrar.Location = new Point(754, 117);
+            BtCadastrar.Name = "BtCadastrar";
+            BtCadastrar.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            BtCadastrar.Size = new Size(110, 26);
+            BtCadastrar.TabIndex = 31;
+            BtCadastrar.Text = "Cadastrar";
+            BtCadastrar.Click += BtCadastrar_Click;
+            // 
+            // BtMenu
+            // 
+            BtMenu.DisabledState.BorderColor = Color.DarkGray;
+            BtMenu.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtMenu.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtMenu.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtMenu.FillColor = Color.Fuchsia;
+            BtMenu.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            BtMenu.ForeColor = Color.White;
+            BtMenu.Location = new Point(24, 22);
+            BtMenu.Name = "BtMenu";
+            BtMenu.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            BtMenu.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            BtMenu.Size = new Size(74, 43);
+            BtMenu.TabIndex = 30;
+            BtMenu.Text = "Menu";
+            // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             label4.Location = new Point(767, 61);
             label4.Name = "label4";
-            label4.Size = new Size(23, 15);
+            label4.Size = new Size(24, 15);
             label4.TabIndex = 28;
             label4.Text = "Id :";
             // 
@@ -107,7 +198,7 @@
             // 
             dataGridView1.AllowUserToOrderColumns = true;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(391, 163);
+            dataGridView1.Location = new Point(426, 163);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
             dataGridView1.Size = new Size(650, 173);
@@ -126,6 +217,7 @@
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             label3.Location = new Point(347, 121);
             label3.Name = "label3";
             label3.Size = new Size(47, 15);
@@ -158,19 +250,21 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI Emoji", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.Font = new Font("Showcard Gothic", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.ForeColor = Color.Fuchsia;
             label6.Location = new Point(552, 15);
             label6.Name = "label6";
-            label6.Size = new Size(267, 32);
+            label6.Size = new Size(267, 30);
             label6.TabIndex = 10;
             label6.Text = "FICHA DE CADASTRO ";
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             label1.Location = new Point(347, 62);
             label1.Name = "label1";
-            label1.Size = new Size(46, 15);
+            label1.Size = new Size(47, 15);
             label1.TabIndex = 0;
             label1.Text = "Nome :";
             // 
@@ -184,9 +278,10 @@
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             label2.Location = new Point(359, 90);
             label2.Name = "label2";
-            label2.Size = new Size(34, 15);
+            label2.Size = new Size(33, 15);
             label2.TabIndex = 2;
             label2.Text = "CPF :";
             // 
@@ -197,95 +292,11 @@
             TxtCPF.Size = new Size(100, 23);
             TxtCPF.TabIndex = 3;
             // 
-            // BtMenu
-            // 
-            BtMenu.DisabledState.BorderColor = Color.DarkGray;
-            BtMenu.DisabledState.CustomBorderColor = Color.DarkGray;
-            BtMenu.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            BtMenu.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtMenu.FillColor = Color.Fuchsia;
-            BtMenu.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            BtMenu.ForeColor = Color.White;
-            BtMenu.Location = new Point(24, 22);
-            BtMenu.Name = "BtMenu";
-            BtMenu.ShadowDecoration.CustomizableEdges = customizableEdges9;
-            BtMenu.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            BtMenu.Size = new Size(74, 43);
-            BtMenu.TabIndex = 30;
-            BtMenu.Text = "Menu";
-            // 
-            // BtCadastrar
-            // 
-            BtCadastrar.CustomizableEdges = customizableEdges7;
-            BtCadastrar.DisabledState.BorderColor = Color.DarkGray;
-            BtCadastrar.DisabledState.CustomBorderColor = Color.DarkGray;
-            BtCadastrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            BtCadastrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtCadastrar.FillColor = Color.Fuchsia;
-            BtCadastrar.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            BtCadastrar.ForeColor = Color.White;
-            BtCadastrar.Location = new Point(754, 117);
-            BtCadastrar.Name = "BtCadastrar";
-            BtCadastrar.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            BtCadastrar.Size = new Size(110, 26);
-            BtCadastrar.TabIndex = 31;
-            BtCadastrar.Text = "Cadastrar";
-            // 
-            // BtBuscar
-            // 
-            BtBuscar.CustomizableEdges = customizableEdges5;
-            BtBuscar.DisabledState.BorderColor = Color.DarkGray;
-            BtBuscar.DisabledState.CustomBorderColor = Color.DarkGray;
-            BtBuscar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            BtBuscar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtBuscar.FillColor = Color.Fuchsia;
-            BtBuscar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            BtBuscar.ForeColor = Color.White;
-            BtBuscar.Location = new Point(847, 56);
-            BtBuscar.Name = "BtBuscar";
-            BtBuscar.ShadowDecoration.CustomizableEdges = customizableEdges6;
-            BtBuscar.Size = new Size(96, 23);
-            BtBuscar.TabIndex = 32;
-            BtBuscar.Text = "Buscar";
-            // 
-            // BtAtualizar
-            // 
-            BtAtualizar.CustomizableEdges = customizableEdges3;
-            BtAtualizar.DisabledState.BorderColor = Color.DarkGray;
-            BtAtualizar.DisabledState.CustomBorderColor = Color.DarkGray;
-            BtAtualizar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            BtAtualizar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtAtualizar.FillColor = Color.Fuchsia;
-            BtAtualizar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            BtAtualizar.ForeColor = Color.White;
-            BtAtualizar.Location = new Point(949, 56);
-            BtAtualizar.Name = "BtAtualizar";
-            BtAtualizar.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            BtAtualizar.Size = new Size(96, 23);
-            BtAtualizar.TabIndex = 33;
-            BtAtualizar.Text = "Atualizar";
-            // 
-            // BtExcluir
-            // 
-            BtExcluir.CustomizableEdges = customizableEdges1;
-            BtExcluir.DisabledState.BorderColor = Color.DarkGray;
-            BtExcluir.DisabledState.CustomBorderColor = Color.DarkGray;
-            BtExcluir.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            BtExcluir.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtExcluir.FillColor = Color.Fuchsia;
-            BtExcluir.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            BtExcluir.ForeColor = Color.White;
-            BtExcluir.Location = new Point(1051, 56);
-            BtExcluir.Name = "BtExcluir";
-            BtExcluir.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            BtExcluir.Size = new Size(96, 23);
-            BtExcluir.TabIndex = 34;
-            BtExcluir.Text = "Excluir";
-            // 
             // ControleCadastroADM
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
             ClientSize = new Size(1231, 450);
             Controls.Add(groupBox1);
             Name = "ControleCadastroADM";
--- LuMoura.ul/ControleCadastroADM.cs
+++ LuMoura.ul/ControleCadastroADM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,115 @@ namespace LuMoura.ul
         public ControleCadastroADM()
         {
             InitializeComponent();
+        }
+
+        public void Atualizar(string Nome, string CPF, string Telefone, string Email)
+        {
+            //SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Lu_Moura; Integrated Security = True; Connect Timeout = 30; Encrypt = False");
+            //conn.Open();
+
+            SqlConnection conn = new SqlConnection(@"Data Source=FAC0539709W10-1;Initial Catalog=LuMoura.DB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("Update Cliente set Nome = '" + Nome + " ', CPF = '" + CPF + "',Telefone = '" + Telefone + "', Email = '" + Email + "' where IdCliente = " + TxtCodigo.Text, conn);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Atualizado");
+
+            TxtNome.Text = ""; TxtCPF.Text = ""; TxtTelefone.Text = ""; TxtEmail.Text = "";
+        }
+
+        private void BtCadastrar_Click(object sender, EventArgs e)
+        {
+            {
+
+                CadastroDAL objdal = new CadastroDAL();
+                objdal.FichaCadastro(TxtNome.Text, TxtCPF.Text, TxtTelefone.Text, TxtEmail.Text);
+                TxtNome.Text = ""; TxtCPF.Text = ""; TxtTelefone.Text = ""; TxtEmail.Text = ""; TxtCodigo.Text = "";
+
+                dataGridView1.Visible = false;
+                dataGridView1.Visible = true;
+
+            }
+        }
+
+        private void BtBuscar_Click(object sender, EventArgs e)
+        {
+            //Buscar
+
+
+            //SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Lu_Moura; Integrated Security = True; Connect Timeout = 30; Encrypt = False");
+            //conn.Open();  
+
+            SqlConnection conn = new SqlConnection(@"Data Source=FAC0539709W10-1;Initial Catalog=LuMoura.DB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Cliente where idCliente=" + TxtCodigo.Text, conn);
+
+            //var dr = cmd.ExecuteReader();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr == null)
+            {
+                MessageBox.Show("Cliente não Encontrado!");
+            }
+
+            else
+            {
+                if (dr.Read())
+                {
+
+                    TxtNome.Text = dr["Nome"].ToString();
+                    TxtCPF.Text = dr["CPF"].ToString();
+                    TxtTelefone.Text = dr["Telefone"].ToString();
+                    TxtEmail.Text = dr["Email"].ToString();
+
+                }
+
+            }
+        }
+
+        private void BtAtualizar_Click(object sender, EventArgs e)
+        {
+            Atualizar(TxtNome.Text, TxtCPF.Text, TxtTelefone.Text, TxtEmail.Text);
+        }
+
+        private void BtExcluir_Click(object sender, EventArgs e)
+        {
+
+            int IdCliente = Convert.ToInt32(dataGridView1.CurrentRow.Cells["IdCliente"].Value);
+
+            //using (SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Lu_Moura; Integrated Security = True; Connect Timeout = 30; Encrypt = False"))
+
+
+            using (SqlConnection conn = new SqlConnection(@"Data Source=FAC0539709W10-1;Initial Catalog=LuMoura.DB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Cliente WHERE IdCliente = @ID", conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID", IdCliente);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+
+
+                        MessageBox.Show("Cadastro Excluído com Sucesso.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Falha ao Excluir o Cadastro.");
+                    }
+
+                    TxtNome.Text = ""; TxtCPF.Text = ""; TxtTelefone.Text = ""; TxtEmail.Text = ""; TxtCodigo.Text = "";
+                }
+            }
         }
     }
 }
